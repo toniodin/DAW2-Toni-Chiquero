6933b786c6c5f96855ad61aa9b91a737fa575247
toniodin <tonichiqueroencinas@gmail.com>
2022-10-19 17:20:04 +0200

    MP06
    .

--- MP06 Desarrollo web/PTAdicional/src/app/app.component.ts
+++ MP06 Desarrollo web/PTAdicional/src/app/app.component.ts
@@ -1,39 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Character {
+  name: string;
+  strength: number;
+  agility: number;
+  intelligence: number;
+  life: number;
+  editable?: boolean;
+  printar: string;
+  boton: string;
+  mostrar:boolean;
+  boton2:string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  _text: string = "Sample Text";
-  _disabled: boolean = true;
-  _inputText: string = "";
-  Nombres: string[] = ['ErPepe', 'ErMarcos', 'ErAdagi'];
-  fuerza:number = 0;
-  destreza:number =  0;
-  inteligencia:number =  0;
-  constitucion:number =  0;
-  nombre:number = 0;
-  datos = ["",0,0,0,0];
+  serverCharacters: Character[] = [];
+  print: string = '';
+  show = [true, false];
 
 
-  botonClickado(event: any): void {
-    console.log(event)
+  constructor() {
+    // Ejemplo de respuesta de un servidor en formato JSON
+    const serverJson = `[
+      {"name": "Jugger", "strength": 18, "agility": 12, "intelligence": 6, "life": 30, "editable": false, "boton":"Edit","mostrar":true, "boton2":"Hide" },
+      {"name": "Pelegrin", "strength": 20, "agility": 8, "intelligence": 6, "life": 40, "editable": false, "boton":"Edit","mostrar":true, "boton2":"Hide" },
+      {"name": "Dorthak", "strength": 12, "agility": 18, "intelligence": 10, "life": 16, "editable": false, "boton":"Edit","mostrar":true, "boton2":"Hide" },
+      {"name": "Kharak", "strength": 8, "agility": 20, "intelligence": 12, "life": 14, "editable": false, "boton":"Edit","mostrar":true, "boton2":"Hide" },
+      {"name": "Perz", "strength": 10, "agility": 6, "intelligence": 20, "life": 10, "editable": false, "boton":"Edit","mostrar":true, "boton2":"Hide" }
+    ]`;
+
+    // Parseamos la información y la convertimos directamente en un array de "Character"
+    this.serverCharacters = JSON.parse(serverJson);
   }
 
-  generarValoresRandom($event: any, fuerza: number, destreza: number, inteligencia: number, constitucion: number, nombre:number): void {
-    this.nombre =  Math.floor(Math.random() * 3);
-    this.fuerza =  Math.floor(Math.random() * 21);
-    this.destreza =  Math.floor(Math.random() * 21);
-    this.inteligencia =  Math.floor(Math.random() * 21);
-    this.constitucion =  Math.floor(Math.random() * 21);
-    //console.log(nombre);
+  editar(Character: Character, print: string): any {
+    console.log(Character.editable);
+    if (Character.editable == false) {
+      Character.editable = true;
+      Character.boton = "Save";
+    } else {
+      Character.editable = false;
+      Character.boton = "Edit";
+    }
+    Character.printar = ("name: /" + Character.name + "/ strength: /" + Character.strength + "/ agillity: /" + Character.agility + " /intelligence: /" + Character.intelligence + "/ life: /" + Character.life);
+
+    return print = JSON.stringify(Character.printar);
   }
 
-  guardar($event: any, fuerza: number, destreza: number, inteligencia: number, constitucion: number, Nombres:any, nombre:number, datos:any): void{
-    this.datos = [this.Nombres[nombre],this.fuerza,this.destreza,this.inteligencia,this.constitucion];
+  ngOnInit(): void{
+    // this.serverCharacters.push({Character[0]});
+  }
+
+  mostrar(character:Character, print: string): any{
+
+    if(character.mostrar == true){
+      character.mostrar = false;
+      character.boton2 = "Show";
+    }else{
+      character.mostrar = true;
+      character.boton2 = "Hide";
+    }
+
+    return print = JSON.stringify("name: /" + character.name + "/ strength: /" + character.strength + "/ agillity: /" + character.agility + " /intelligence: /" + character.intelligence + "/ life: /" + character.life + "/ mostrar: /" + character.mostrar);
+
   }
 
 }
--- MP06 Desarrollo web/PTAdicional/src/app/app.module.ts
+++ MP06 Desarrollo web/PTAdicional/src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-
 
 @NgModule({
   declarations: [
@@ -12,7 +10,6 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
