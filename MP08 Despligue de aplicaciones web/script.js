c0a65fed9f510d9d8d742a2c351ded4c0de6119e
toniodin <tonichiqueroencinas@gmail.com>
2022-10-20 19:55:22 +0200

    MP08
    fin pt jsdoc

--- MP08 Despligue de aplicaciones web/script.js
+++ MP08 Despligue de aplicaciones web/script.js
@@ -1,21 +1,9 @@
-/** 
- * Esto es una descripción de las funciones
- * @author Toni Chiquero
+
+
+
+/**
+ * @author toni chiquero <jsmith@example.com>
  * @alias Odin
- * @copyright Toni
- * @version 1.0.0
- */
-
-/** 
- * @function esta funcion solo se ejecutará cuando  se inicie la página
- */
-$(document).ready(function() {
-   
-});
-
-/** 
- * @throws esta función te muestra un mensaje de aviso
- * @see { @link https://runebook.dev/es/docs/jsdoc/ | JSDOC}
  */
 function notificarEnvio(){
     swal({
@@ -29,7 +17,7 @@ function notificarEnvio(){
 }
 
 /** 
- * @returns esta función retorna una confirmación
+ * @return cerrarSesion(event) esta función retorna una confirmación
 */
 function cerrarSesion(event) {
     event.preventDefault();
@@ -52,6 +40,11 @@ function cerrarSesion(event) {
     });
 }
 
+/** 
+ * @throws esta función te muestra un mensaje de aviso
+ * @see { @link https://runebook.dev/es/docs/jsdoc/ | JSDOC}
+ * @function  esta funcion solo se ejecutará cuando  se inicie la página
+ */
 
 function notificarUpdate(){
     swal({
