c4746dfd357be81476b1c60149d96bf1d03e313e
toniodin <tonichiqueroencinas@gmail.com>
2022-10-25 19:24:44 +0200

    MP06
    awa

--- MP06 Desarrollo web/PruebaPT_Calendario/src/app/app.component.ts
+++ MP06 Desarrollo web/PruebaPT_Calendario/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 interface Dia {
-  Dia:string
+  name: string;
 }
 
 @Component({
@@ -15,22 +15,23 @@ export class AppComponent {
   octubre = [,,,,,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
   semana = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
   mes = ["Septiembre","Octubre"];
+  tipo = ["Festivo Nacional","Festivo Regional","Festivo Local","Festivo Centro","Dia Laborable"];
 
-  tipoDia: Dia[] = [];
+  // tipoDia: Dia[] = [];
 
 
-  constructor() {
-    // Ejemplo de respuesta de un servidor en formato JSON
-    const serverJson = `[
-      {"Dia": "Festivo Nacional"},
-      {"Dia": "Festivo Regional"},
-      {"Dia": "Festivo Local"},
-      {"Dia": "Festivo Centro"},
-      {"Dia": "Dia Laborable"}
-    ]`;
+  // constructor() {
+  //   // Ejemplo de respuesta de un servidor en formato JSON
+  //   const serverJson = `[
+  //     {"name": "Festivo Nacional"},
+  //     {"name": "Festivo Regional"},
+  //     {"name": "Festivo Local"},
+  //     {"name": "Festivo Centro"},
+  //     {"name": "Dia Laborable"}
+  //   ]`;
 
-    // Parseamos la información y la convertimos directamente en un array de "Character"
-    this.tipoDia = JSON.parse(serverJson);
-  }
+  //   // Parseamos la información y la convertimos directamente en un array de "Character"
+  //   this.tipoDia = JSON.parse(serverJson);
+  // }
 
 }
