72db425c4ac24a8015611327163e8d4d225f5791
toniodin <tonichiqueroencinas@gmail.com>
2022-11-03 19:56:20 +0100

    MP07
    Hecho insert por mi cuenta en laravel

--- MP07 Desarrolo web en entorno servidor/Laravel/PrimerProyecto-app/app/Http/Controllers/Api/UserController.php
+++ MP07 Desarrolo web en entorno servidor/Laravel/PrimerProyecto-app/app/Http/Controllers/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Products;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -24,6 +25,67 @@ class UserController extends Controller
         return response()->json([
             "status" => 1,
             "msg" => "¡Registro de usuario exitoso!",
+        ]);
+    }
+
+     public function login(Request $request) {
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+        $user = User::where("email", "=", $request->email)->first();
+        if( isset($user->id) ){
+            if(Hash::check($request->password, $user->password)){
+                //creamos el token
+                $token = $user->createToken("auth_token")->plainTextToken;
+                //si está todo ok
+                return response()->json([
+                    "status" => 1,
+                    "msg" => "¡Usuario logueado exitosamente!",
+                    "access_token" => $token
+                ]);
+            }else{
+                return response()->json([
+                    "status" => 0,
+                    "msg" => "La password es incorrecta",
+                ], 404);
+            }
+        }else{
+            return response()->json([
+                "status" => 0,
+                "msg" => "Usuario no registrado",
+            ], 404);
+        }
+    }
+
+    public function userProfile() {
+        return response()->json([
+            "status" => 0,
+            "msg" => "Acerca del perfil de usuario",
+            "data" => auth()->user()
+        ]);
+    }
+
+    public function logout() {
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            "status" => 1,
+            "msg" => "Cierre de Sesión",
+        ]);
+    }
+
+    public function addProduct(Request $request) {
+        $request->validate([
+            'name' => 'required',
+            'precio' => 'required'
+        ]);
+        $product = new Products();
+        $product->name = $request->name;
+        $product->precio = $request->precio;
+        $product->save();
+        return response()->json([
+            "status" => 1,
+            "msg" => "¡Registro de producto exitoso!",
         ]);
     }
 }
--- MP07 Desarrolo web en entorno servidor/Laravel/PrimerProyecto-app/routes/api.php
+++ MP07 Desarrolo web en entorno servidor/Laravel/PrimerProyecto-app/routes/api.php
@@ -15,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+Route::post('addProduct', [UserController::class, 'addProduct']);
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group( ['middleware' => ["auth:sanctum"]], function(){
+    //rutas
+    Route::get('user-profile', [UserController::class, 'userProfile']);
+    Route::get('logout', [UserController::class, 'logout']);
 });
