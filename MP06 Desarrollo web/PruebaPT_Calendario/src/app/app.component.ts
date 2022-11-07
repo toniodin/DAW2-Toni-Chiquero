0e320f59422058841a9e35c104a52ff0c0ddc14a
toniodin <tonichiqueroencinas@gmail.com>
2022-11-07 20:11:01 +0100

    MP06
    me cago en cristo

--- MP06 Desarrollo web/PruebaPT_Calendario/src/app/app.component.ts
+++ MP06 Desarrollo web/PruebaPT_Calendario/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 
-interface meses {
+interface mes {
 
-  festivos: string[];
   diasFestivos: number[];
-  diasMes: number[];
+  dias: number;
   mes: string[];
 
 }
@@ -19,28 +18,29 @@ interface meses {
 
 export class AppComponent {
 
-  meses:meses[] = [];
-  // septiembre = [,,,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
-  // octubre = [,,,,,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+  meses: mes[] = [];
   semana = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
-  // mes = ["Septiembre","Octubre"];
-  // tipo = ["Festivo Nacional","Festivo Regional","Festivo Local","Festivo Centro","Dia Laborable"];
+  festivos = ["Fiesta Nacional", "Fiesta Regional", "Fiesta Local", "Fiesta Centro"];
 
   constructor() {
     // Ejemplo de respuesta de un servidor en formato JSON
     const serverJson = `[
-      {"diasMes": "30", "dias[]":"[]","diasFestivos[]":"[]","mes":"septiembre"},
-      {"diasMes": "31", "dias[]":"diasFestivos[],"":[],"mes":"octubre"}
+      {"dias": 30, "diasFestivos": [], "mes": "septiembre"},
+      {"dias": 31, "diasFestivos": [], "mes": "octubre"}
     ]`;
 
-    // Parseamos la información y la convertimos directamente en un array de "Character"
     this.meses = JSON.parse(serverJson);
+    // console.log(this.meses);
   }
 
-  ngOnInit(meses:meses):void{
-    for(let i = 0; meses.diasMes;i++){
-      meses.diasMes.push(i);
+  rellenarDias(mes: mes): number[] {
+    let diasMes: number[] = [];
+
+    for(let i = 1; i <= mes.dias; i++){
+      diasMes.push(i);
     }
+
+    return diasMes;
   }
 
 }
