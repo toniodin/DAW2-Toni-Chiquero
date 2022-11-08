157563829e1284b079ef87519247317347ff44ea
toniodin <tonichiqueroencinas@gmail.com>
2022-11-08 20:19:25 +0100

    MP06
    falltan los festivos

--- MP06 Desarrollo web/PruebaPT_Calendario/src/app/app.component.ts
+++ MP06 Desarrollo web/PruebaPT_Calendario/src/app/app.component.ts
@@ -6,7 +6,8 @@ interface mes {
   diasFestivos: number[];
   dias: number;
   mes: string[];
-
+  diasMes: any[];
+  diaInicio: number;
 }
 
 
@@ -19,28 +20,36 @@ interface mes {
 export class AppComponent {
 
   meses: mes[] = [];
-  semana = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+  semana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
   festivos = ["Fiesta Nacional", "Fiesta Regional", "Fiesta Local", "Fiesta Centro"];
+  mes: mes[] = [
+
+  ];
 
   constructor() {
     // Ejemplo de respuesta de un servidor en formato JSON
     const serverJson = `[
-      {"dias": 30, "diasFestivos": [], "mes": "septiembre"},
-      {"dias": 31, "diasFestivos": [], "mes": "octubre"}
+      {"dias": 30, "diasFestivos": [11,29], "mes": "septiembre", "diasMes": [], "diaInicio": 3},
+      {"dias": 31, "diasFestivos": [12,31], "mes": "octubre", "diasMes": [], "diaInicio": 5}
     ]`;
 
     this.meses = JSON.parse(serverJson);
-    // console.log(this.meses);
+
+
+    console.log(this.meses);
+    this.rellenarDias();
+
   }
 
-  rellenarDias(mes: mes): number[] {
-    let diasMes: number[] = [];
+  rellenarDias(): void {
 
-    for(let i = 1; i <= mes.dias; i++){
-      diasMes.push(i);
+    for (let mes of this.meses) {
+      for (let j = 0; j < mes.diaInicio; j++) {
+        mes.diasMes.push(null);
+      }
+      for (let i = 1; i < mes.dias +1; i++) {
+        mes.diasMes.push(i);
+      }
     }
-
-    return diasMes;
   }
-
 }
