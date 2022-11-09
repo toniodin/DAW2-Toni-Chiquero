434a9b69bc71a08497b239561394da66850d48dc
toniodin <tonichiqueroencinas@gmail.com>
2022-11-09 20:07:49 +0100

    Mp06
    fin pt calendario

--- MP06 Desarrollo web/PruebaPT_Calendario/src/app/app.component.ts
+++ MP06 Desarrollo web/PruebaPT_Calendario/src/app/app.component.ts
@@ -3,11 +3,17 @@ import { Component, OnInit } from '@angular/core';
 
 interface mes {
 
-  diasFestivos: number[];
   dias: number;
-  mes: string[];
+  mes: string;
   diasMes: any[];
   diaInicio: number;
+
+}
+
+interface dia {
+  idDia: string;
+  mes?: string[];
+  diasFestivos: any;
 }
 
 
@@ -22,18 +28,26 @@ export class AppComponent {
   meses: mes[] = [];
   semana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
   festivos = ["Fiesta Nacional", "Fiesta Regional", "Fiesta Local", "Fiesta Centro"];
-  mes: mes[] = [
-
-  ];
+  idDia: string[] = ["l","r","n","c"];
+  mes: string[] = ["septiembre", "octubre"];
+  dia: dia [] = [];
 
   constructor() {
     // Ejemplo de respuesta de un servidor en formato JSON
     const serverJson = `[
-      {"dias": 30, "diasFestivos": [11,29], "mes": "septiembre", "diasMes": [], "diaInicio": 3},
-      {"dias": 31, "diasFestivos": [12,31], "mes": "octubre", "diasMes": [], "diaInicio": 5}
+      {"dias": 30, "mes": "septiembre", "diasMes": [], "diaInicio": 3},
+      {"dias": 31, "mes": "octubre", "diasMes": [], "diaInicio": 5}
+    ]`;
+
+    const serverJson2 = `[
+      {"mes": "septiembre", "diasFestivos": [{ "dia": 29, "idDia": "l"}]},
+      {"mes": "septiembre", "diasFestivos": [{ "dia": 11, "idDia": "r"}]},
+      {"mes": "octubre", "diasFestivos": [{ "dia": 12, "idDia": "n"}]},
+      {"mes": "octubre", "diasFestivos": [{ "dia": 31, "idDia": "c"}]}
     ]`;
 
     this.meses = JSON.parse(serverJson);
+    this.dia = JSON.parse(serverJson2);
 
 
     console.log(this.meses);
@@ -52,4 +66,21 @@ export class AppComponent {
       }
     }
   }
+
+  // printarDias(mes:mes, dia:dia) {
+
+  //   for (let i = 1; i < mes.diasMes.length; i++) {
+  //     if(dia.diasFestivos[i].idDia === 'l' && dia.diasFestivos[i].dia == 29 && mes.mes == 'septiembre'){
+  //       return 'local';
+  //     }else if(dia.diasFestivos[i].idDia === 'r' && dia.diasFestivos[i].dia == 11 && mes.mes == 'septiembre'){
+  //       return 'regional';
+  //     }else if(dia.diasFestivos[i].idDia === 'n' && dia.diasFestivos[i].dia == 12 && mes.mes == 'octubre'){
+  //       return 'nacional';
+  //     }else if(dia.diasFestivos[i].idDia === 'c' && dia.diasFestivos[i].dia == 31 && mes.mes == 'octubre'){
+  //       return 'centro';
+  //     }
+      
+  //   }
+  //   return '';
+  // }
 }
