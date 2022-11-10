c9dea7ccc71ec89b51f6d03454e97939e1ca056e
toniodin <tonichiqueroencinas@gmail.com>
2022-11-10 19:58:58 +0100

    MP07
    ya he creado las tablas, recuerda que no te sirve subirlo a git, bobo

---
+++ MP07 Desarrolo web en entorno servidor/Laravel/segundoProyecto/database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->word(),
+            'description' => fake()->realText(),
+            'price' => rand(1,50),
+            'stock' => rand(1,9999),
+        ];
+    }
+}
---
+++ MP07 Desarrolo web en entorno servidor/Laravel/segundoProyecto/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+
+Route::group( ['middleware' => ["auth:sanctum"]], function(){
+    //rutas
+    Route::get('user-profile', [UserController::class, 'userProfile']);
+    Route::get('logout', [UserController::class, 'logout']);
+    Route::post('insert-product', [ProductController::class, 'insert']);
+
+});
