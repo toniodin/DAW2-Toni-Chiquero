284d6de9f5a2979378545b5096f4d79ecfab1023
toniodin <tonichiqueroencinas@gmail.com>
2022-11-11 20:10:26 +0100

    MP07
    Fin de la PT CRUD de laravel

---
+++ MP07 Desarrolo web en entorno servidor/Laravel/segundoProyecto/segundoProyecto/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // User::factory(100)->create();
+        // Product::factory(100)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
