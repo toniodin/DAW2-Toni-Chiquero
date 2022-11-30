d932fd9289e38e901ceabed2300e8ef3cff9b851
toniodin <tonichiqueroencinas@gmail.com>
2022-11-30 20:18:15 +0100

    xd
    xd

--- MP06 Desarrollo web/Kanban Board2/src/app/tarea-form/tarea-form.component.ts
+++ MP06 Desarrollo web/Kanban Board2/src/app/tarea-form/tarea-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { Tarea } from '../models/tarea-model';
 
 @Component({
   selector: 'app-tarea-form',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarea-form.component.css']
 })
 export class TareaFormComponent implements OnInit {
+  
+  @Output() form: Tarea;
 
-  constructor() { }
+  constructor() { 
+    this.form = {id: 0 ,lista: "", img: "", titulo: "", usuarios: [],fechaFin:new Date()}
+  }
 
   ngOnInit(): void {
   }
